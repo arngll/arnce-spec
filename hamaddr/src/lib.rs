aaa2e710e7011d7b0aaf68a159c2d39293dbd33c
Dan Cross <cross@gajendra.net>
2022-01-05 19:15:57 +0000

    Address review comments from @darconeous
    Signed-off-by: Dan Cross <cross@gajendra.net>

--- hamaddr/src/lib.rs
+++ hamaddr/src/lib.rs
@@ -1,25 +1,22 @@
-type Result<T> = std::result::Result<T, String>;
+use std::convert::TryInto;
+use std::fmt;
+use anyhow::{bail, format_err};
+
+type Result<T> = std::result::Result<T, anyhow::Error>;
 
 /// Eui48 represents an EUI48 MAC address.
-pub struct Eui48([u8; 6]);
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
+pub struct Eui48(pub [u8; 6]);
 
 impl Eui48 {
     /// Creates a new Eui48 from the given six octets.
     pub const fn new(addr: [u8; 6]) -> Eui48 {
         Eui48(addr)
     }
-
-    /// Returns a string representing the Eui48.
-    pub fn to_hex_string(&self) -> String {
-        let bs = &self.0;
-        format!(
-            "{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}",
-            bs[0], bs[1], bs[2], bs[3], bs[4], bs[5]
-        )
-    }
-
-    /// Returns a HamAddr from the Eui48.
-    pub fn to_ham_addr(&self) -> HamAddr {
+}
+/// Converts an Eui48 into a HamAddr
+impl Into<HamAddr> for Eui48 {
+    fn into(self: Self) -> HamAddr {
         let mut octets = self.0;
         octets[0] &= 0b1111_1101;
         octets.rotate_left(1);
@@ -30,26 +27,44 @@ impl Eui48 {
     }
 }
 
+/// Formats an Eui48 for display.
+impl fmt::Display for Eui48 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let bs = &self.0;
+        write!(
+            f,
+            "{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}",
+            bs[0], bs[1], bs[2], bs[3], bs[4], bs[5]
+        )
+    }
+}
+
 /// Eui64 represents an EUI64 MAC address.
-pub struct Eui64([u8; 8]);
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
+pub struct Eui64(pub [u8; 8]);
 
 impl Eui64 {
     /// Creates a new Eui64 from the given 8 octets.
     pub const fn new(addr: [u8; 8]) -> Eui64 {
         Eui64(addr)
     }
+}
 
-    /// Returns a string representing the Eui64.
-    pub fn to_hex_string(&self) -> String {
+/// Formats an Eui64 for display.
+impl fmt::Display for Eui64 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let bs = &self.0;
-        format!(
+        write!(
+            f,
             "{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}:{:02x}",
             bs[0], bs[1], bs[2], bs[3], bs[4], bs[5], bs[6], bs[7]
         )
     }
+}
 
-    /// Returns a HamAddr from the Eui64.
-    pub fn to_ham_addr(&self) -> HamAddr {
+/// Converts an Eui64 into a HamAddr.
+impl Into<HamAddr> for Eui64 {
+    fn into(self: Self) -> HamAddr {
         let mut bytes = self.0;
         bytes[0] &= 0b1111_1101;
         if bytes[3] == 0xFF && bytes[4] == 0xFE {
@@ -89,21 +104,22 @@ mod eui_tests {
     }
 
     #[test]
-    fn test_to_hex_str_eui48() {
+    fn test_to_str_eui48() {
         let eui = Eui48::new([1, 2, 3, 4, 5, 6]);
-        let s = eui.to_hex_string();
+        let s = eui.to_string();
         assert_eq!(s, "01:02:03:04:05:06");
     }
 
     #[test]
-    fn test_to_hex_str_eui64() {
+    fn test_to_str_eui64() {
         let eui = Eui64::new([1, 2, 3, 4, 5, 6, 0x77, 0x88]);
-        let s = eui.to_hex_string();
+        let s = eui.to_string();
         assert_eq!(s, "01:02:03:04:05:06:77:88");
     }
 }
 
 /// An ARNCE encoded address
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Hash)]
 pub struct HamAddr(u64);
 
 impl HamAddr {
@@ -114,7 +130,7 @@ impl HamAddr {
         fn char_to_index(c: char) -> Result<u16> {
             HamAddr::CHARS
                 .find(c)
-                .ok_or(format!("bad char '{}'", c))
+                .ok_or(format_err!("bad char '{}'", c))
                 .map(|k| k as u16 + 1)
         }
         fn chunk_to_u16(chunk: &[char]) -> u16 {
@@ -128,11 +144,11 @@ impl HamAddr {
         }
         for c in callsign.to_uppercase().chars() {
             if !HamAddr::CHARS.contains(c) {
-                return Err(format!("Callsign contains bad character '{}'", c));
+                bail!("Callsign contains bad character '{}'", c);
             }
         }
         if callsign.len() > 12 {
-            return Err(format!("Callsign '{}' is too long", callsign));
+            bail!("Callsign '{}' is too long", callsign);
         }
         let numeric = callsign
             .to_uppercase()
@@ -172,39 +188,17 @@ impl HamAddr {
             .join("")
     }
 
-    /// Returns a hex string representing the HamAddr.
-    pub fn to_hex_string(&self) -> String {
-        let cs = [
-            (self.0 >> 48) as u16,
-            (self.0 >> 32) as u16,
-            (self.0 >> 16) as u16,
-            self.0 as u16,
-        ];
-        cs.iter()
-            .filter(|c| **c != 0)
-            .map(|c| format!("{:04X}", *c))
-            .collect::<Vec<_>>()
-            .join(":")
-    }
-
     /// Returns a HAM64 encoded string corresponding to the HamAddr.
     pub fn to_ham64_string(&self) -> String {
-        let cs = [
-            (self.0 >> 48) as u16,
-            (self.0 >> 32) as u16,
-            (self.0 >> 16) as u16,
-            self.0 as u16,
-        ];
-        cs.iter()
-            .map(|c| format!("{:04X}", *c))
-            .collect::<Vec<_>>()
-            .join("-")
+        format!("{:#}", self)
     }
+}
 
-    /// Attempts to convert the HamAddr to an Eui64.
-    pub fn to_eui64(&self) -> Result<Eui64> {
+impl TryInto<Eui64> for HamAddr {
+    type Error = anyhow::Error;
+    fn try_into(self: Self) -> Result<Eui64> {
         if self.0 & 0b0111 != 0 {
-            return Err("HamAddr too big".to_string());
+            bail!("HamAddr too big");
         }
         let is_small = self.0 & 0x0000_0000_0007_FFFF == 0;
         let mut bytes = self.0.to_be_bytes();
@@ -218,17 +212,39 @@ impl HamAddr {
         }
         Ok(Eui64::new(bytes))
     }
+}
 
-    /// Attempts to convert the HamAddr to an Eui48.
-    pub fn to_eui48(&self) -> Result<Eui48> {
+impl TryInto<Eui48> for HamAddr {
+    type Error = anyhow::Error;
+    fn try_into(self: Self) -> Result<Eui48> {
         if self.0 & 0x0000_0000_0007_FFFF != 0 {
-            return Err("HamAddr too big".to_string());
+            bail!("HamAddr too big");
         }
         let mut bytes = [0, 0, 0, 0, 0, 0];
         bytes.copy_from_slice(&self.0.to_be_bytes()[..6]);
         bytes.rotate_right(1);
         bytes[0] = (bytes[0] & 0b1111_1000) | 0b0010;
         Ok(Eui48::new(bytes))
+    }
+}
+
+impl fmt::Display for HamAddr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            [
+                (self.0 >> 48) as u16,
+                (self.0 >> 32) as u16,
+                (self.0 >> 16) as u16,
+                self.0 as u16,
+            ]
+            .iter()
+            .filter(|c| f.alternate() || **c != 0)
+            .map(|c| format!("{:04X}", *c))
+            .collect::<Vec<_>>()
+            .join(if f.alternate() { "-" } else { ":" })
+        )
     }
 }
 
@@ -245,14 +261,14 @@ mod ham_addr_tests {
     #[test]
     fn test_ham_addr_to_hex_string() {
         let addr = HamAddr::parse_callsign("KZ2X-1").unwrap();
-        assert_eq!(addr.to_hex_string(), "48ED:9C0C");
+        assert_eq!(addr.to_string(), "48ED:9C0C");
 
         let addr = HamAddr::parse_callsign("N6DRC").unwrap();
-        assert_eq!(addr.to_hex_string(), "5CAC:70F8");
+        assert_eq!(addr.to_string(), "5CAC:70F8");
         assert_eq!(addr.to_ham64_string(), "5CAC-70F8-0000-0000");
 
         let addr = HamAddr::parse_callsign("VI2BMARC50").unwrap();
-        assert_eq!(addr.to_hex_string(), "8B05:0E89:7118:A8C0");
+        assert_eq!(addr.to_string(), "8B05:0E89:7118:A8C0");
 
         // TODO(cross): This was one of the test vectors from the
         // ARNCE specification, but the encoded ham64 string does
@@ -266,57 +282,57 @@ mod ham_addr_tests {
     #[test]
     fn test_ham_addr_to_eui64() {
         let addr = HamAddr::parse_callsign("KZ2X-1").unwrap();
-        let eui64 = addr.to_eui64().unwrap();
-        assert_eq!(eui64.to_hex_string(), "02:48:ed:ff:fe:9c:0c:00");
-        let addr = eui64.to_ham_addr();
+        let eui64: Eui64 = addr.try_into().unwrap();
+        assert_eq!(eui64.to_string(), "02:48:ed:ff:fe:9c:0c:00");
+        let addr: HamAddr = eui64.into();
         assert_eq!(addr.to_callsign(), "KZ2X-1");
-        let eui64 = addr.to_eui64().unwrap();
-        assert_eq!(eui64.to_hex_string(), "02:48:ed:ff:fe:9c:0c:00");
+        let eui64: Eui64 = addr.try_into().unwrap();
+        assert_eq!(eui64.to_string(), "02:48:ed:ff:fe:9c:0c:00");
 
         let addr = HamAddr::parse_callsign("AC2OI").unwrap();
-        let eui64 = addr.to_eui64().unwrap();
-        assert_eq!(eui64.to_hex_string(), "02:06:d5:ff:fe:5f:28:00");
+        let eui64: Eui64 = addr.try_into().unwrap();
+        assert_eq!(eui64.to_string(), "02:06:d5:ff:fe:5f:28:00");
 
         let addr = HamAddr::parse_callsign("WB3KUZ-111").unwrap();
-        let eui64 = addr.to_eui64().unwrap();
-        assert_eq!(eui64.to_hex_string(), "02:90:2e:48:22:f1:fc:af");
+        let eui64: Eui64 = addr.try_into().unwrap();
+        assert_eq!(eui64.to_string(), "02:90:2e:48:22:f1:fc:af");
 
         let addr = HamAddr::parse_callsign("VI2BMARC50").unwrap();
-        let eui64 = addr.to_eui64().unwrap();
-        assert_eq!(eui64.to_hex_string(), "c2:8b:05:0e:89:71:18:a8");
-        let addr = eui64.to_ham_addr();
+        let eui64: Eui64 = addr.try_into().unwrap();
+        assert_eq!(eui64.to_string(), "c2:8b:05:0e:89:71:18:a8");
+        let addr: HamAddr = eui64.into();
         assert_eq!(addr.to_callsign(), "VI2BMARC50");
-        let eui64 = addr.to_eui64().unwrap();
-        assert_eq!(eui64.to_hex_string(), "c2:8b:05:0e:89:71:18:a8");
+        let eui64: Eui64 = addr.try_into().unwrap();
+        assert_eq!(eui64.to_string(), "c2:8b:05:0e:89:71:18:a8");
     }
 
     #[test]
     fn test_ham_addr_to_eui48() {
         let addr = HamAddr::parse_callsign("KZ2X-1").unwrap();
-        let eui48 = addr.to_eui48().unwrap();
-        assert_eq!(eui48.to_hex_string(), "02:48:ed:9c:0c:00");
+        let eui48: Eui48 = addr.try_into().unwrap();
+        assert_eq!(eui48.to_string(), "02:48:ed:9c:0c:00");
 
-        let addr = eui48.to_ham_addr();
+        let addr: HamAddr = eui48.into();
         assert_eq!(addr.to_callsign(), "KZ2X-1");
 
         let addr = HamAddr::parse_callsign("AC2OI").unwrap();
-        let eui48 = addr.to_eui48().unwrap();
-        assert_eq!(eui48.to_hex_string(), "02:06:d5:5f:28:00");
+        let eui48: Eui48 = addr.try_into().unwrap();
+        assert_eq!(eui48.to_string(), "02:06:d5:5f:28:00");
 
         let addr = HamAddr::parse_callsign("WB3KUZ-1").unwrap();
-        let eui48 = addr.to_eui48().unwrap();
-        assert_eq!(eui48.to_hex_string(), "e2:90:2e:48:22:f1");
-        let addr = eui48.to_ham_addr();
+        let eui48: Eui48 = addr.try_into().unwrap();
+        assert_eq!(eui48.to_string(), "e2:90:2e:48:22:f1");
+        let addr: HamAddr = eui48.into();
         assert_eq!(addr.to_callsign(), "WB3KUZ-1");
 
         let addr = HamAddr::parse_callsign("NA1SS").unwrap();
-        let eui48 = addr.to_eui48().unwrap();
-        assert_eq!(eui48.to_hex_string(), "02:57:c4:79:b8:00");
-        let addr = eui48.to_ham_addr();
+        let eui48: Eui48 = addr.try_into().unwrap();
+        assert_eq!(eui48.to_string(), "02:57:c4:79:b8:00");
+        let addr: HamAddr = eui48.into();
         assert_eq!(addr.to_callsign(), "NA1SS");
 
         let addr = HamAddr::parse_callsign("VI2BMARC50").unwrap();
-        let not_eui48 = addr.to_eui48();
+        let not_eui48: Result<Eui48> = addr.try_into();
         assert!(matches!(not_eui48, Err(_)));
     }
 }
